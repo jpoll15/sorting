672b4370c78aa39ffb1c9ebe008cb006a88ec46d
jpoll15 <jpollack015@gmail.com>
2019-01-14 11:19:55 -0600

    added spy attempt

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,9 +1,20 @@
 describe('the bubble sorting algorithm', function(){
   let test_sort_arr = [6,3,2,5,10,4,9,8,0,1,7]
   let test_sorted_arr = [0,1,2,3,4,5,6,7,8,9,10]
+
+  beforeEach(() => {
+    spyOn(window, 'swap').and.callThrough()
+  })
+
   it('can sort an array', function(){
-    sorted_arr = bubbleSort(test_sort_arr)
+    const sorted_arr = bubbleSort(test_sort_arr)
 
     expect(JSON.stringify(sorted_arr)).toEqual(JSON.stringify(test_sorted_arr))
+
+
+  })
+  it('uses swap', function(){
+
+    expect(swap.calls.count()).toEqual(55)
   })
 })
