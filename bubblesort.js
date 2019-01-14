10b3eb0922c55a276719467f99111cd4d840680b
jpoll15 <jpollack015@gmail.com>
2019-01-14 13:38:50 -0600

    formatted

--- bubblesort.js
+++ bubblesort.js
@@ -1,16 +1,17 @@
-function swap(array, i){
+function swap (array, i) {
   let current = array[i];
   let next = array[i+1];
-  if(current>next){
+  if (current > next) {
     array[i] = next;
     array[i+1] = current
-}}
+  }
+}
 
 function bubbleSort (array){
-  for(let l = array.length -1; l>0; l--){
-    for(let i = 0; i<l; i++){
-        swap(array,i)
-        }
-      }
-    return array;
+  for (let l = array.length - 1; l > 0; l--) {
+    for (let i = 0; i < l; i++) {
+      swap(array,i)
+    }
   }
+  return array;
+}
